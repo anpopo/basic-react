9b7b5435605d6af49e7dfa35160e567b74886920
anpopo <dkstpgud@gmail.com>
2021-07-14 22:56:30 +0900

    ref

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Counter from './component/Counter';
 import Counter2 from './component/Counter2';
 import MyComponent from './component/MyComponent';
@@ -11,29 +11,38 @@ import EventPractice4 from './eventhandling/EventPractice4';
 import EventPractice5 from './eventhandling/EventPractice5';
 import EventPractice6 from './eventhandling/EventPractice6';
 import EventPractice7 from './eventhandling/EventPractice7';
+import ValidationSample from './ref/ValidationSample';
+import ScrollBox from './ref/ScrollBox';
 
-function App() {
-    return (
-        <>
-            <MyComponent name={`anpopo`}>
-                <MyComponentClass favoriteNumber={44} />
-                <br />
-                <Counter />
-                <Counter2 />
-                <br />
-                <Say />
-            </MyComponent>
-            <hr />
-
-            <EventPractice />
-            <EventPractice2 />
-            <EventPractice3 />
-            <EventPractice4 />
-            <EventPractice5 />
-            <EventPractice6 />
-            <EventPractice7 />
-        </>
-    );
+class App extends Component {
+    render() {
+        return (
+            <>
+                {/* <MyComponent name={`anpopo`}>
+                    <MyComponentClass favoriteNumber={44} />
+                    <br />
+                    <Counter />
+                    <Counter2 />
+                    <br />
+                    <Say />
+                </MyComponent>
+                <hr />
+    
+                <EventPractice />
+                <EventPractice2 />
+                <EventPractice3 />
+                <EventPractice4 />
+                <EventPractice5 />
+                <EventPractice6 />
+                <EventPractice7 /> */}
+                <ValidationSample />
+                <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+                <button onClick={() => this.scrollBox.scrollToBottom()}>
+                    맨밑
+                </button>
+            </>
+        );
+    }
 }
 
 export default App;
